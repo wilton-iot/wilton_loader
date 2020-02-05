3431c9d020d264571dae4f4c60de01f4a813dde5
alex <alex@staticlibs.net>
2020-02-05 21:40:28 +0000

    support loading binary resources

--- src/wiltoncall_loader.cpp
+++ src/wiltoncall_loader.cpp
@@ -36,14 +36,40 @@ namespace wilton {
 namespace loader {
 
 support::buffer load_module_resource(sl::io::span<const char> data) {
+    // json parse
+    auto json = sl::json::load(data);
+    auto rurl = std::ref(sl::utils::empty_string());
+    auto hex = false;
+    for (const sl::json::field& fi : json.as_object()) {
+        auto& name = fi.name();
+        if ("url" == name) {
+            rurl = fi.as_string_nonempty_or_throw(name);
+        } else if ("hex" == name) {
+            hex = fi.as_bool_or_throw(name);
+        } else {
+            throw support::exception(TRACEMSG("Unknown data field: [" + name + "]"));
+        }
+    }
+    if (rurl.get().empty()) throw support::exception(TRACEMSG(
+            "Required parameter 'url' not specified"));
+    const std::string& url = rurl.get();
+    // call wilton
     char* out = nullptr;
     int out_len = 0;
-    char* err = wilton_load_resource(data.data(), data.size_int(),
+    char* err = wilton_load_resource(url.c_str(), static_cast<int> (url.length()),
             std::addressof(out), std::addressof(out_len));
     if (nullptr != err) {
         support::throw_wilton_error(err, TRACEMSG(err));
     }
-    return support::wrap_wilton_buffer(out, out_len);
+    if (hex) {
+        auto deferred = sl::support::defer([out]() STATICLIB_NOEXCEPT {
+            wilton_free(out);
+        });
+        auto src = sl::io::array_source(out, out_len);
+        return support::make_hex_buffer(src);
+    } else {
+        return support::wrap_wilton_buffer(out, out_len);
+    }
 }
 
 } // namespace
